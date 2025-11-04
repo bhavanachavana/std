21c76b21214f47a0ead8b521a35b32d3167ad727
bhavanachavana <bhavanachavana7@gmail.com>
2025-11-04 11:55:03 +0530

    Fix variable names and improve input handling
    Correct variable names and formatting for clarity.

--- stdinfo.py
+++ stdinfo.py
@@ -1,15 +1,16 @@
 import sys
-if len(sys.argv)==3:
+
+if len(sys.argv) ==3:
 script name = sys.argv[0]
 name=sys.argv[1]
 roll no= sys.argv[2]
 print(" user provided input values:")
 
 else:
- script_name = sys.argv[0]
+ script name = sys.argv[0]
 name="bhavana"
 rollno= "015"
 print("no input given - using default values :")
-print("script name :",script_name)
+print("script name :",script name)
 print("student name :",name)
 print("roll no :",roll no)
