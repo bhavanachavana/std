8257e10ba2a78cc7640be4e1956107a16d6682f3
bhavanachavana <bhavanachavana7@gmail.com>
2025-11-04 11:34:11 +0530

    Correct input validation and default values in stdinfo.py
    Fix input handling and variable assignments in stdinfo.py

---
+++ stdinfo.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv)==3:
+script name = sys.argv[0]
+name=sys.argv[1]
+roll no= sys.argv[2]
+print(" user provided input values:")
+
+else:
+ script_name = sys.argv[0]
+name="bhavana"
+rollno= "015"
+print("no input given - using default values :")
+print("script name :",script_name)
+print("student name :",name)
+print("roll no :",roll no)
