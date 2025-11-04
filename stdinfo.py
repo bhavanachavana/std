68690438ce8ff42b3343b9f7dfb86e1e7c7335f0
bhavanachavana <bhavanachavana7@gmail.com>
2025-11-04 12:05:17 +0530

    Update stdinfo.py

--- stdinfo.py
+++ stdinfo.py
@@ -1,16 +1,16 @@
-import sys
+ import sys
 
 if len(sys.argv) == 3:
-script_name = sys.argv[0]
-name = sys.argv[1]
-rollno = sys.argv[2]
-print(" user provided input values:")
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User provided input values:")
 else:
-script_name = sys.argv[0]
-name = "bhavana"
-rollno = "015"
-print("no input given - using default values :")
+    script_name = sys.argv[0]
+    name = "Bhavana"
+    rollno = "015"
+    print("No input given - using default values:")
 
-print("script name :",script_name)
-print("student name :",name)
-print("roll no :",rollno)
+print("Script Name:", script_name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
