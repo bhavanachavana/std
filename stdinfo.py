fa929003679431e05c08befd3e69ca43ae13746a
bhavanachavana <bhavanachavana7@gmail.com>
2025-11-04 11:59:06 +0530

    Update stdinfo.py

--- stdinfo.py
+++ stdinfo.py
@@ -7,9 +7,9 @@ roll no= sys.argv[2]
 print(" user provided input values:")
 
 else:
- script name = sys.argv[0]
+script name = sys.argv[0]
 name="bhavana"
-rollno= "015"
+roll no= "015"
 print("no input given - using default values :")
 print("script name :",script name)
 print("student name :",name)
